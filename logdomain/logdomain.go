f5fa85ee7cd737f246fe2081509a9fe2c4f0ab54
Benjamin Walkenhorst <krylon@posteo.de>
2026-03-09 13:11:38 +0100

    add package logdomain

---
+++ logdomain/logdomain.go
@@ -0,0 +1,31 @@
+// /home/krylon/go/src/github.com/blicero/newsroom/logdomain/logdomain.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 09. 03. 2026 by Benjamin Walkenhorst
+// (c) 2026 Benjamin Walkenhorst
+// Time-stamp: <2026-03-09 13:11:30 krylon>
+
+package logdomain
+
+//go:generate stringer -type=ID
+
+// ID signifies a part of the application that wants to write to the log.
+type ID uint8
+
+const (
+	Database
+	Engine
+	Classifier
+	UI
+	Main
+)
+
+// All returns a slice of all valid ID values.
+func All() []ID {
+	return []ID{
+		Database,
+		Engine,
+		Classifier,
+		UI,
+		Main,
+	}
+} // func All() []ID
