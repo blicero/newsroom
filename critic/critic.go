3449ecbc9c56adbffa012927c2e43ba702a5fe34
Benjamin Walkenhorst <krylon@posteo.de>
2026-03-19 18:06:11 +0100

    critic - start actually writing some code (not much, though)

--- critic/critic.go
+++ critic/critic.go
@@ -2,18 +2,22 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 16. 03. 2026 by Benjamin Walkenhorst
 // (c) 2026 Benjamin Walkenhorst
-// Time-stamp: <2026-03-16 19:04:25 krylon>
+// Time-stamp: <2026-03-19 17:22:46 krylon>
 
 // Package critic deals with guessing the most probable rating for Items.
 // Like a spam filter for news.
 package critic
 
 import (
+	"fmt"
 	"log"
+	"path/filepath"
 	"sync"
 	"time"
 
+	"github.com/blicero/newsroom/common"
 	"github.com/blicero/newsroom/database"
+	"github.com/blicero/newsroom/logdomain"
 	"github.com/blicero/shield"
 )
 
@@ -23,6 +27,8 @@ const (
 	errTmp       = "resource temporarily unavailable"
 )
 
+var languages = []string{"en", "de"}
+
 // Critic passes judgement on news Items, whether they be
 // Interesting or Boring.
 type Critic struct {
@@ -31,3 +37,43 @@ type Critic struct {
 	db      *database.Database
 	lock    sync.RWMutex
 }
+
+// New creates and returns a fresh Critic instance.
+func New() (*Critic, error) {
+	var (
+		err error
+		c   = new(Critic)
+	)
+
+	if c.log, err = common.GetLogger(logdomain.Critic); err != nil {
+		return nil, err
+	} else if c.db, err = database.Open(common.DbPath); err != nil {
+		c.log.Printf("[CRITICAL] Failed to open database at %s: %s\n",
+			common.DbPath,
+			err.Error())
+		return nil, err
+	}
+
+	for _, lng := range languages {
+		var (
+			tok       shield.Tokenizer
+			storePath = filepath.Join(common.CachePath, fmt.Sprintf("critic_store_%s", lng))
+			store     = shield.NewLevelDBStore(storePath)
+		)
+
+		switch lng {
+		case "en":
+			tok = shield.NewEnglishTokenizer()
+		case "de":
+			tok = shield.NewGermanTokenizer()
+		default:
+			c.log.Printf("[CANTHAPPEN] Unsupported language %s\n",
+				lng)
+			return nil, fmt.Errorf("unsupported language %s", lng)
+		}
+
+		c.critics[lng] = shield.New(tok, store)
+	}
+
+	return c, nil
+} // func New() (*Critic, error)
