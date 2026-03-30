50462d6ebfa26fc82f4e56963bcf32fb2bf70f1a
Benjamin Walkenhorst <krylon@posteo.de>
2026-03-30 21:15:57 +0200

    rating - add method FromString

--- model/rating/rating.go
+++ model/rating/rating.go
@@ -2,10 +2,15 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 09. 03. 2026 by Benjamin Walkenhorst
 // (c) 2026 Benjamin Walkenhorst
-// Time-stamp: <2026-03-09 13:50:33 krylon>
+// Time-stamp: <2026-03-30 21:09:08 krylon>
 
 // Package rating defines constants for rating news Items.
 package rating
+
+import (
+	"fmt"
+	"strings"
+)
 
 //go:generate stringer -type=Rating
 
@@ -17,3 +22,17 @@ const (
 	Boring
 	Interesting
 )
+
+// FromString returns the Rating expressed by the given string.
+func FromString(s string) (Rating, error) {
+	switch strings.ToLower(s) {
+	case "unrated":
+		return Unrated, nil
+	case "boring":
+		return Boring, nil
+	case "interesting":
+		return Interesting, nil
+	default:
+		return Unrated, fmt.Errorf("Invalid Rating: %q", s)
+	}
+} // func FromString(s string) (Rating, error)
