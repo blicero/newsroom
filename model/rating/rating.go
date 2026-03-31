9ebeac51c1bc4213756ab8e7dcb00eb11ab976f5
Benjamin Walkenhorst <krylon@posteo.de>
2026-03-31 15:27:47 +0200

    model/rating - parse "unknown" as "unrated"

--- model/rating/rating.go
+++ model/rating/rating.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 09. 03. 2026 by Benjamin Walkenhorst
 // (c) 2026 Benjamin Walkenhorst
-// Time-stamp: <2026-03-30 21:09:08 krylon>
+// Time-stamp: <2026-03-31 15:01:35 krylon>
 
 // Package rating defines constants for rating news Items.
 package rating
@@ -27,6 +27,8 @@ const (
 func FromString(s string) (Rating, error) {
 	switch strings.ToLower(s) {
 	case "unrated":
+		return Unrated, nil
+	case "unknown":
 		return Unrated, nil
 	case "boring":
 		return Boring, nil
