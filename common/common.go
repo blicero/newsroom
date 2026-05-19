b6d88cd802f46e5e0aae08098f92a6ee1e1c8d6c
Benjamin Walkenhorst <krylon@posteo.de>
2026-05-19 11:46:56 +0200

    bump version to 0.9.1

--- common/common.go
+++ common/common.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 09. 03. 2026 by Benjamin Walkenhorst
 // (c) 2026 Benjamin Walkenhorst
-// Time-stamp: <2026-05-18 15:28:31 krylon>
+// Time-stamp: <2026-05-19 11:46:49 krylon>
 
 package common
 
@@ -36,7 +36,7 @@ import (
 // application.
 const (
 	AppName                  = "Newsroom"
-	Version                  = "0.9.0"
+	Version                  = "0.9.1"
 	Debug                    = true
 	TimestampFormatMinute    = "2006-01-02 15:04"
 	TimestampFormat          = "2006-01-02 15:04:05"
